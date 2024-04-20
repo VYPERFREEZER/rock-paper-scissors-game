804c62836d9067148c7c77ab4ff001b2fdae9366
vyper freezer <williammuchui584@gmail.com>
2024-04-20 16:21:09 +0300

    prints results for every instance

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -18,29 +18,35 @@ class Game:
         player_choice = input("Rock Paper Scissors (r, p, s): ").lower()
         return player_choice
     
-    def player_win(self):
+    def player_win(self, computer_choice, player_choice):
         print("You win!!")
+        #print choices
+        print(f"{player_choice} > {computer_choice}\n")
         self.player_points += 1
 
-    def computer_win(self):
+    def computer_win(self, computer_choice, player_choice):
         print("The computer wins!!")
+        #print choices
+        print(f"{computer_choice} > {player_choice}\n")
         self.computer_points += 1
         
-    def draw(self):
+    def draw(self, computer_choice, player_choice):
         print("Draw!!")
+        #print choices
+        print(f"{computer_choice} == {player_choice}\n")
     
     def check_winner(self, player_choice, computer_choice):
         # Player wins
         if (player_choice == "r" and computer_choice == "s") or (player_choice == "p" and computer_choice == "r") or (player_choice == "s" and computer_choice == "p"):
-            self.player_win()
+            self.player_win(player_choice, computer_choice)
 
         # Computer wins
         elif (computer_choice == "r" and player_choice == "s") or (computer_choice == "p" and player_choice == "r") or (computer_choice == "s" and player_choice == "p"):
-            self.computer_win()
+            self.computer_win(computer_choice, player_choice)
             
         # Draw
         elif computer_choice == player_choice:
-            self.draw()
+            self.draw(computer_choice, player_choice)
             
         # Quit game
         elif player_choice == "q":
@@ -48,7 +54,7 @@ class Game:
             
         # Invalid response
         else:
-            print("Invalid Response!!")
+            print("Invalid Response!!\n")
     
     def quit_game(self):
         print("=" * 20)
@@ -68,9 +74,10 @@ class Game:
         print("=" * 20)
         sys.exit()
         
+    #initialise the game
     def start_game(self):
         accept = self.intro()
-        while accept != "y" and accept != "n":
+        while accept not in ["y", "n", "q"]:
             print("**" * 20)
             print("Invalid input")
             print("**" * 20)
